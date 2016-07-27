a638377af6de137ea95cb10a8616b99bd13b1049
Rosales <rosalk2@covidien.com>
2016-07-27 09:23:25 -0600

    C sharp programs

---
+++ Statements/Conditions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un caracter");
+            char c = (Char)Console.Read();
+            //c = Convert.ToChar(Console.Read());
+
+            //Condicion tipo if
+
+            if (Char.IsLetter(c)) {
+                if (Char.IsLower(c)) {
+
+                   Console.WriteLine("El caracter no es mayuscula");
+
+                }
+                else
+                {
+                    Console.WriteLine("El caracter es mayuscula");
+
+                }
+
+                Console.WriteLine("El caracter si es una letra");
+
+            }
+            else
+            {
+                Console.WriteLine("El caracter no es una letra");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Statements/Program/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool procced = true;
+            while (procced)
+            {
+              char selecction =  printMenu();
+                switch (selecction)
+                {
+                    case 'a':
+                        addValues();
+                        break;
+                    case 'b':
+                        ConcatenateStrings();
+                        break;
+                    case 'c':
+                        LoopNumber();
+                        break;
+                    case 'd':
+                        procced = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opcion Incorrecta.");
+
+                        break;
+                }
+            }
+        }
+
+        private static void LoopNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ConcatenateStrings()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void addValues()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static char printMenu()
+        {
+            Console.WriteLine("Seleccione una opcion");
+            Console.WriteLine("a - Sumar dos valores");
+            Console.WriteLine("b - Concatenar dos String");
+            Console.WriteLine("c - Iterar un numero");
+            Console.WriteLine("e - Salir");
+            char option = (char)Console.Read();
+            Console.WriteLine();
+            return option;
+        }
+    }
+}
